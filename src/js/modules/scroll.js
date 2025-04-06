e39f57affe207fd83a37c0110394247f64ed9ef0
Aliaksandr Bykouski <developweb34@gmail.com>
2025-04-06 22:36:08 +0200

    add scroll

--- src/js/modules/scroll.js
+++ src/js/modules/scroll.js
@@ -9,6 +9,98 @@ const scrolling = (upSelector) => {
             upElem.classList.remove('fadeIn');
         }
     });
+
+    // плавный скролл до якоря с помощью requestAnimationFrame
+
+    let links = document.querySelectorAll('[href^="#"]'),
+        speed = 0.3;
+
+    links.forEach(link => {
+        link.addEventListener('click', function(event) {
+            event.preventDefault();
+            let widthTop = document.documentElement.scrollTop,
+                hash = this.hash,
+                toBlock = document.querySelector(hash).getBoundingClientRect().top,
+                start = null;
+
+            requestAnimationFrame(step);
+
+            function step(time) {
+                if (start === null) {
+                    start = time;
+                }
+
+                let progress = time - start,
+                    r = (toBlock < 0 ? Math.max(widthTop - progress / speed, widthTop + toBlock) : Math.min(widthTop + progress / speed, widthTop + toBlock));
+
+                document.documentElement.scrollTo(0 , r);
+
+                if (r != widthTop + toBlock) {
+                    requestAnimationFrame(step);
+                } else {
+                    location.hash = hash;
+                }
+            }
+        });
+    });
+
+    // плавный скролл до якоря обычным способом
+
+    // const element = document.documentElement,
+    //     body = document.body;
+    //
+    // let calcScrollValue = () => {
+    //     upElem.addEventListener('click', function(event) {
+    //         let scrollTop = Math.round(body.scrollTop || element.scrollTop);
+    //         if (this.hash !== '') {
+    //             event.preventDefault();
+    //             let hashEl = document.querySelector(this.hash),
+    //                 hashElTop = 0;
+    //
+    //             while (hashEl.offsetParent) {
+    //                 hashElTop += hashEl.offsetTop;
+    //                 hashEl = hashEl.offsetParent;
+    //             }
+    //
+    //             hashElTop = Math.round(hashElTop);
+    //             smoothScroll(scrollTop, hashElTop, this.hash);
+    //         }
+    //     });
+    // };
+    //
+    // const smoothScroll = (from, to, hash) => {
+    //     let timeInterval = 1,
+    //         prevScrollTop,
+    //         speed;
+    //
+    //     if (to > from) {
+    //         speed = 30;
+    //     } else {
+    //         speed = -30;
+    //     }
+    //
+    //     let move = setInterval(function() {
+    //         let scrollTop = Math.round(body.scrollTop || element.scrollTop);
+    //         if (
+    //             prevScrollTop === scrollTop ||
+    //             (to > from && scrollTop >= to) ||
+    //             (to < from && scrollTop <= to)
+    //         ) {
+    //             clearInterval(move);
+    //             history.replaceState(
+    //                 history.state,
+    //                 document.title,
+    //                 location.href.replace(/#.*$/g, '') + hash
+    //             );
+    //         } else {
+    //             body.scrollTop += speed;
+    //             element.scrollTop += speed;
+    //             prevScrollTop = scrollTop;
+    //         }
+    //     }, timeInterval);
+    // };
+    //
+    // calcScrollValue();
 };
 
 export default scrolling;
